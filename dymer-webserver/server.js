670b536f2a257ee65109f34b252f44f26a58c4f0
Marco Romano <marcoromano12@gmail.com>
2022-07-28 10:09:37 +0200

    Update server.js

--- dymer-webserver/server.js
+++ dymer-webserver/server.js
@@ -223,8 +223,51 @@ app.post(util.getContextPath('webserver') + '/api2/retriveinfo', loadUserInfo, (
     // console.log("api retriveinfo", JSON.stringify(objuser));
     logger.info(nameFile + ' | /api2/retriveinfo :' + JSON.stringify(objuser));
     res.send(objuser);
-
-
+});
+app.get(util.getContextPath('webserver') + '/version', (req, res, next) => {
+    var pjson = require('./package.json');
+    let htmlsend =
+        '<!DOCTYPE html>' +
+        '<html lang="en"><head>' +
+        '<meta charset="utf-8">' +
+        '<meta http-equiv="X-UA-Compatible" content="IE=edge">' +
+        '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">' +
+        '<meta name="description" content="DYnamic Information ModElling & Rendering">' +
+        '<meta name="author" content="marco romano">' +
+        '<title> DYMER</title> ' +
+        '<link rel="icon" type="image/png" href="https://raw.githubusercontent.com/Engineering-Research-and-Development/DYMER/master/screenshots/dymer-logo.png"/>' +
+        '<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">' +
+        '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">' +
+        '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>' +
+        '<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous" ></script>' +
+        '<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>' + '</script>' +
+        ' </head > ' +
+        '<body  style="background-color:#ebecf2;"> <div class="container"> ' +
+        '<div class="row justify-content-center">' +
+        ' <div class="col-xl-10 col-lg-12 col-md-9" > ' +
+        '<div class="card o-hidden border-0 shadow-lg my-5">' +
+        '<div class="card-body p-0">' +
+        '<div class="row">' +
+        '<div class="col-lg-6 d-none d-lg-block bg-login-image" style=\'background:url("https://raw.githubusercontent.com/Engineering-Research-and-Development/DYMER/master/screenshots/bg-ver.jpg");background-position: center;background-size: cover; min-height: 280px; \'>' + '</div>' +
+        '<div class="col-lg-6">' +
+        '<div class="p-5">' + '<div class="row">' +
+        '<div class=" col-12">' +
+        '<h1 class="h4   mb-4 text-center"  style="color:#023d7d;">Welcome to DYMER</h1>' +
+        '<div class="text-center">' + '<img src="https://raw.githubusercontent.com/Engineering-Research-and-Development/DYMER/master/screenshots/dymer-logo.png" style="width: 45px;" >' + '<div>' +
+        '<br><small style="color: #8c8985;">DYnamic Information ModElling & Rendering</small>' +
+        '</div>' +
+        '<div class="  	col-12 p-2" style="color: #8c8985;">' +
+        '<br> version ' + pjson.version + '</div>' +
+        '<div class="text-center col-12 p-2">' +
+        '<span style=" font-size: 12px;">&#169; 2022, Prowered by <a href="https://www.eng.it/" target="_blank">' + '<img src="https://www.eng.it/resources/images/logo%20eng.png" style="width: 20px;bottom: 3px;position: relative; "> Enginnering</a>' + '</span>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' + '</div>' + '</div>' + '</div>' + '</body>' + '</html>';
+    res.send(htmlsend);
 });
 /*
 app.use(session({
@@ -258,7 +301,7 @@ function loadUserInfo(req, res, next) {
     //  console.log('TESTSESSION  req.protocol', req.protocol);
     //console.log('TESTSESSION req host', req.get('host'));
     logger.info(nameFile + ' | loadUserInfo : TESTSESSION req host, originalUrl' + req.get('host') + " , " + req.originalUrl);
-    // console.log('TESTSESSION req originalUrl', req.originalUrl);
+
     var authuserUrl = util.getServiceUrl("dservice") + "/api/v1/authconfig/userinfo";
     var dymtoken = (req.headers.authorization != undefined) ? req.headers.authorization.split(' ')[1] : undefined;
     var dymtokenAT = req.headers.authorizationtk;
