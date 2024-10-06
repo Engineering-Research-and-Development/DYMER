509f6d234fef0e9a38402bec2e301d80af77af41
Francesco Stefanelli <francesco.ste@gmail.com>
2024-10-06 17:11:07 +0200

    dropzone

--- dymer-webserver/public/assets/js/kmsweb.js
+++ dymer-webserver/public/assets/js/kmsweb.js
@@ -111,27 +111,7 @@ $(document).ready(function() {
 			}
 		});
 	});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
     $(".form-control").on('focus', function(){
 		var tmpThis = $(this).val();
 		if(tmpThis == '' ) {
@@ -153,7 +133,7 @@ $(document).ready(function() {
 	});
 
     $(".form-control").on('focus', function(){
-		var tmpThis = jQuery(this).val();
+		var tmpThis = $(this).val();
 		if(tmpThis == '' ) {
 			$(this).parent().addClass("focus-input");
 		}
@@ -197,7 +177,7 @@ $(document).ready(function() {
             handleSelection(event);
           }
         });
- });
+    });
 
 });
 
@@ -214,4 +194,62 @@ function urlToPromiseZip(url) {
             }
         });
     });
+}
+
+
+//selecting all required elements
+const dropArea = document.querySelector(".drag-area"),
+dragText = dropArea.querySelector("header"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file; //this is a global variable and we'll use it inside multiple functions
+
+button.onclick = ()=>{
+  input.click(); //if user click on the button then the input also clicked
+}
+
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
+});
+
+
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
+
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+});
+
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = [".csv", "csv" ]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+   }
+    fileReader.readAsDataURL(file);
+    getFieldByCSV();
+  }else{
+    alert("This is not a CSV File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
 }
