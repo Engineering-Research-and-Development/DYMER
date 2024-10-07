157858a5246dad775f9bf634f33cf0986c67d7c0
Francesco Stefanelli <francesco.ste@gmail.com>
2024-10-07 14:12:05 +0200

    dark

--- dymer-webserver/public/assets/js/kmsweb.js
+++ dymer-webserver/public/assets/js/kmsweb.js
@@ -55,7 +55,17 @@ $(document).ready(function() {
  
 });
 
-
+function toggleDarkMode() {
+    let isDark = document.body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
+  }
+  
+  // On page load
+  document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+      document.body.classList.add('dark-mode');
+    }
+  });
 
    
 function urlToPromiseZip(url) {
