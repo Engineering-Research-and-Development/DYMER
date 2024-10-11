117343be742e67cbc933f7c43d8c6ca7ab22eeb1
Francesco <francesco.ste@gmail.com>
2024-10-11 16:53:41 +0200

    Update routes-v2.js

--- dymer-entities/routes/routes-v2.js
+++ dymer-entities/routes/routes-v2.js
@@ -5523,8 +5523,15 @@ router.patch("/like-entity", async (req, res) => {
     const entityId = req.body.element
     try {
         const entityArray = await getAllEntitiesFromIDS([entityId])
-        let entity = entityArray[0]
-        let likesArray = JSON.parse(entity["_source"]["likes"])
+       let entity = entityArray[0];
+        let likesArray;
+        if(entity["_source"]["likes"] !== undefined){
+            likesArray = JSON.parse(entity["_source"]["likes"])
+        }else{
+            likesArray=[];
+        }
+        
+        console.log("********************* likesArray -->>>>",likesArray);
         if (likesArray.indexOf(userEmail) === -1) {
             likesArray.push(userEmail)
             ret.setMessages({action: "like", count: likesArray.length, likes: likesArray})
